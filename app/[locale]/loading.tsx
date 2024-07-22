3c1165cb894ca7817c481630ff89c26e795edf2b
Bruno Delic <92450207+BDelic11@users.noreply.github.com>
2024-07-22 14:31:49 +0200

    fix: small fix loader

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -4,13 +4,7 @@ export default function Loading() {
   return (
     <main className="min-h-screen flex flex-col  items-center">
       <div className="m-auto pt-4 text-off-white text-2xl md:pt-16 lg:pt-20">
-        <Audio
-          height="80"
-          width="80"
-          radius="9"
-          color="white"
-          ariaLabel="loading"
-        />
+        <Audio height="80" width="80" color="white" ariaLabel="loading" />
       </div>
     </main>
   );
