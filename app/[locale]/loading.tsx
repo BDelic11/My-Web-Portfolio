b04f32d75a9de18377213053febd52f1f485b3df
Bruno Delic <92450207+BDelic11@users.noreply.github.com>
2024-07-22 14:30:36 +0200

    Added loader

--- app/[locale]/loading.tsx
+++ app/[locale]/loading.tsx
@@ -1,9 +1,17 @@
+"use client";
+import { Audio } from "react-loader-spinner";
 export default function Loading() {
   return (
     <main className="min-h-screen flex flex-col  items-center">
-      <p className="m-auto pt-6 text-off-white text-2xl md:pt-24 lg:pt-40">
-        Loading...
-      </p>
+      <div className="m-auto pt-4 text-off-white text-2xl md:pt-16 lg:pt-20">
+        <Audio
+          height="80"
+          width="80"
+          radius="9"
+          color="white"
+          ariaLabel="loading"
+        />
+      </div>
     </main>
   );
 }
